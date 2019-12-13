bc3ed1bf305fa4e5399a7b7275240bdb82868d94
Ni Made Virginia <madevirginia06@gmail.com>
2019-12-13 09:13:42 +0800

    [add] gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tamus
   resources :suppliers
   resources :tipepakets
   resources :pakets
   resources :invoices
   resources :bankaccounts
+  root "tamus#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
